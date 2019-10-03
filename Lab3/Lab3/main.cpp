1303c570720f1e03f5723a11bc49169c949ec2c2
unknown <C00213756@itcarlow.ie>
2019-10-03 09:31:39 +0100

    Added Lab 3

---
+++ Lab3/Lab3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <SDL_image.h>
+#include <SDL.h>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	
+	SDL_Quit();
+	return 0;
+}

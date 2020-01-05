4c91529c852d414d349bdecdf9f3f69c3b86b66d
BrianONeill97 <C00213756@itcarlow.ie>
2020-01-05 15:30:33 +0000

    Added Erlang Files

--- Lab 5 - Testing/TestProject/MainlineTest.cpp
+++ Lab 5 - Testing/TestProject/MainlineTest.cpp
@@ -11,8 +11,8 @@ int main()
 //	p.someMethod();
 	assert(("True",p.someMethod() == true));
 	std::cout << "Assertion Passed";
-	assert(("Failed", p.someMethod() == true));
-	std::cout << "Assertion Passed";
+	assert(("Failed", p.someMethod() == false));
+	std::cout << "Assertion Failed";
 
 	std::cin.get();
 	system("pause");
